0b79c4cd46127eb93895a608d933ac048cb35507
Matthias Smith <mwsmith2@uw.edu>
2014-05-06 14:29:23 -0700

    Removed setup.sh for python library.  Modified .gitignore in python directory to ignore build.

--- python/setup.py
+++ python/setup.py
@@ -2,14 +2,14 @@
 
 # Setup script for my FID analysis library
 
-from distutils.core import Setup
+from distutils.core import setup
 
 setup(
-	name='FID',
+	name='fid',
 	version='0.0.1',
 	description='FID Analysis Library',
 	author='Matthias Smith',
 	author_email='mwsmith2@uw.edu',
 	url='www.github.com/mwsmith2/fid-analysis',
-	packages=['fid'],
+	py_modules=['fid'],
 	)
--- python/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# NOTE: Run this from the directory with fid.py
-# A script that allows access to the python fid library
-
-export PYTHONPATH=$PYTHONPATH:$PWD
