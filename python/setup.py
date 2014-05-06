959aa2633600012fb5fbc50415f001815c447a09
Matthias Smith <mwsmith2@uw.edu>
2014-05-06 14:22:41 -0700

    Added a simple program that saves quadratic gradient FIDs as text files and a setup script for the python lib.

---
+++ python/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# Setup script for my FID analysis library
+
+from distutils.core import Setup
+
+setup(
+	name='FID',
+	version='0.0.1',
+	description='FID Analysis Library',
+	author='Matthias Smith',
+	author_email='mwsmith2@uw.edu',
+	url='www.github.com/mwsmith2/fid-analysis',
+	packages=['fid'],
+	)
