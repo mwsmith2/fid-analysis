7eaf508cff76bddd67e794f397592a6cc12166bb
Matthias W. Smith <mwsmith2@uw.edu>
2014-11-03 11:17:37 -0800

    Updated generate_root_fids.cxx to work with new library format.

--- examples/src/generate_root_fids.cxx
+++ examples/src/generate_root_fids.cxx
@@ -21,37 +21,40 @@ Detail: This is a new test program for my FID libraries
 #include "fid.h"
 
 using namespace fid;
-using namespace fid::sim;
 
 int main(int argc, char **argv)
 {
   // initialize the configurable parameters
-  load_params(argc, argv);
+  if (argc > 1) load_params(argv[1]);
 
   // some necessary parameters
   vec wf;
   vec tm;
-  wf.reserve(len_fids);
-  tm.reserve(len_fids);
+  wf.reserve(sim::num_samples);
+  tm.reserve(sim::num_samples);
   double delta_b;
-  double max_grad = 500; // in ppm
+  double max_grad = 500; // in ppb
+  int num_fids = 100;
   double freq_0 = sim::freq_larmor;
 
-  construct_time_vector(len_fids, i_time, d_time, tm);
+  double final_time = sim::start_time + sim::num_samples*sim::delta_time;
+  tm = construct_range(sim::start_time, sim::delta_time, final_time);
 
   // Set up the ROOT tree to hold the results
   TFile pf("sim_fids.root", "recreate");
   TTree pt("t", "FID Tree");
+  cout.precision(12);
 
   pt.Branch("db", &delta_b, "delta_b/D");
-  pt.Branch("fid", &wf[0], TString::Format("fid[%d]/D", len_fids));
-  pt.Branch("tm", &tm[0], TString::Format("time[%d]/D", len_fids));
+  pt.Branch("fid", &wf[0], TString::Format("fid[%d]/D", sim::num_samples));
+  pt.Branch("tm", &tm[0], TString::Format("time[%d]/D", sim::num_samples));
 
   // begin grad sims
+  cout << "num_fids: " << num_fids << endl;
   for (int i = -1 * num_fids / 2; i < num_fids / 2 + 1; ++i){
 
     delta_b = max_grad * 2.0 * i / num_fids;
-    sim::freq_larmor = (1.0 + 1.0e-6 * delta_b) * freq_0;
+    sim::freq_larmor = (1.0 + 1.0e-9 * delta_b) * freq_0;
 
     std::cout << sim::freq_larmor << std::endl;
 
