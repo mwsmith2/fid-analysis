069d1f6220d6df6255189746c53fc68abe6dbd8a
Matthias W. Smith <mwsmith2@uw.edu>
2015-01-11 18:13:16 -0800

    Fixed an off by one error in the fftfreq function.

--- cpp/src/fid_math.cxx
+++ cpp/src/fid_math.cxx
@@ -116,7 +116,7 @@ vec dsp::fftfreq(const int N, const double dt)
 
 		freq.resize((N + 1) / 2);
 
-		for (int i = 0; i < (N + 1) / 2 + 1; ++i){
+		for (int i = 0; i < (N + 1) / 2; ++i){
 			freq[i] = i / (dt * N);
 		}
 	}
