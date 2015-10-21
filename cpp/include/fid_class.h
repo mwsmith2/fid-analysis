9a5cf58665f2e6c4f2420f51c33bd58f8b14d3d8
Matthias W. Smith <mwsmith2@uw.edu>
2015-10-20 22:11:53 -0500

    Added a FastFid class that just does Zero Counting.

--- cpp/include/fid_class.h
+++ cpp/include/fid_class.h
@@ -140,6 +140,72 @@ class FID {
   Method ParseMethod(const std::string& m);
   
 }; // FID
+
+class FastFid {
+
+ public:
+  
+  // ctors
+  FastFid(const std::string& fid_file);
+  FastFid(const char* fid_file);
+  FastFid(const std::vector<double>& wf, const std::vector<double>& tm);
+  FastFid(const std::vector<double>& wf);
+
+  // Simplified frequency extraction
+  double CalcFreq();
+  double GetFreq();
+  double GetFreqError();
+  
+  // diagnostic function
+  void PrintDiagnosticInfo();
+  void PrintDiagnosticInfo(std::iostream out);
+
+  // accessors
+  const bool& isgood() const { return isgood_; };
+  const std::vector<double>& wf() const { return wf_; };
+  const std::vector<double>& tm() const { return tm_; };
+  const double& freq() const {  return freq_;  }
+  const double& freq_err() const { return freq_err_; };
+  const double fid_time() const { return tm_[f_wf_] - tm_[i_wf_]; };
+  const double snr() const { return pow(max_amp_ / noise_, 2); };
+  const TGraph& gr_time_series() const { return gr_time_series_; };
+  const unsigned int& i_wf() { return i_wf_; };
+  const unsigned int& f_wf() { return f_wf_; };
+
+ private:
+  
+  // Private Member Variables
+  bool isgood_;
+  unsigned int i_wf_; // start and stop of relevant data
+  unsigned int f_wf_;
+  double noise_;
+  double max_amp_;
+  double mean_;
+  double chi2_; // Store the most recent chi2
+  double freq_;
+  double freq_err_;
+  Method freq_method_;
+
+  std::vector<double> guess_;
+  TGraph gr_time_series_;
+  TGraph gr_freq_series_;
+  
+  // bigger data arrays
+  std::vector<double> wf_;
+  std::vector<double> tm_;
+  std::vector<double> temp_; // for random transformations
+
+  // Private Member Functions  
+  // init function to be called after wf_ and tm_ are set.
+  void Init();
+
+  // internal utility functions
+  void CalcNoise();
+  void CalcMaxAmp();      
+  void CenterFid();
+  void FindFidRange();
+  
+}; // FID
  
 } // fid
 
